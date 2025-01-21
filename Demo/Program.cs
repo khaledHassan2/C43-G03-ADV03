ffc134c4c587e7d1bba35ef44cb951092e81b365
khaledHassan2 <khaledamalb22@gmail.com>
2025-01-21 14:17:25 +0200

    V02)Stack

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -18,6 +18,24 @@
             //}
 
             #endregion
+            #region Stack
+            ////first inp last out
+            //Stack<int>Num = new Stack<int>();
+            //Num.Push(1);
+            //Num.Push(2);
+            //Num.Push(3);
+
+            //Console.WriteLine(Num.Pop()); //3
+            //Console.WriteLine(Num.Pop()); //2
+            //Console.WriteLine(Num.Pop()); //1
+
+            //Console.WriteLine(Num.TryPop(out int lnum));
+
+            //foreach (var item in Num)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
