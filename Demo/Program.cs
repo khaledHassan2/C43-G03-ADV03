2a4ae0e27363968f18a131db5442679acbb7ee67
khaledHassan2 <khaledamalb22@gmail.com>
2025-01-21 14:15:42 +0200

    V01)LinkedList

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region LinkedList
+            //LinkedList<int> Numbers = new LinkedList<int>();
+            //Numbers.AddFirst(1);
+            //Numbers.AddAfter(Numbers.First, 2);
+            //Numbers.AddLast(2);
+            //LinkedListNode<int> sec = Numbers.Find(2);
+            //Numbers.AddBefore(sec, 22);
+
+            //foreach (int item in Numbers)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion
         }
     }
 }
